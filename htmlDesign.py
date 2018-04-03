fdca3f27f741b5dcbe5cf8e25c1414b44585bb9c
jacobRettig <jacob13rettig@gmail.com>
2018-04-03 00:07:45 -0500

    Initial non-working version

---
+++ htmlDesign.py
@@ -0,0 +1,6 @@
+#Primary Author: Salem Abuammer
+
+
+#individual file testing
+if __name__ == '__main__':
+    pass
---
+++ sqlHandler.py
@@ -0,0 +1,6 @@
+#Primary Author: James Shelley
+
+
+#individual file testing
+if __name__ == '__main__':
+    pass
