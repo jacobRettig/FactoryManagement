26fee6fd49cbe330c3771c41f5101b0946000349
Salem Abuammer <sabuammer@gmail.com>
2018-04-21 16:39:13 -0500

    Added 'New Product' and 'My Products' pages along with forms to create a new product and new categories

--- odc_app/forms.py
+++ odc_app/forms.py
@@ -3,7 +3,7 @@
 
 #imports
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, BooleanField, SubmitField, TextAreaField, validators
+from wtforms import StringField, PasswordField, BooleanField, SubmitField, TextAreaField, DecimalField, SelectField, validators
 from wtforms.validators import DataRequired, EqualTo, Email, ValidationError, Length
 from wtforms.fields.html5 import DateField
 import phonenumbers
@@ -20,17 +20,38 @@ class RegistrationForm(FlaskForm):
 	submit = SubmitField('Register')
 
 	def validate_phone(self, phone):
-		if not phone.data.isdigit() or len(phone.data) > 16:
+		if len(phone.data) > 16:
 			raise ValidationError('Invalid phone number.')
 		try:
 			input_number = phonenumbers.parse(phone.data)
 			if not (phonenumbers.is_valid_number(input_number)):
 				raise ValidationError('Invalid phone number.')
 		except:
-			raise ValidationError('Invalid phone number.')
+			input_number = phonenumbers.parse("+1{}".format(phone.data))
+			if not (phonenumbers.is_valid_number(input_number)):
+				raise ValidationError('Invalid phone number.')
 
 class LoginForm(FlaskForm):
 	username = StringField('Username', validators=[DataRequired()])
 	password = PasswordField('Password', validators=[DataRequired()])
 	remember_me = BooleanField('Remember Me')
 	submit = SubmitField('Sign In')
+
+class CreateProductForm(FlaskForm):
+	product_name = StringField("Product Name", validators=[DataRequired()])
+	""" The below are just a few exmaple default categories, there will also be a separate form that allows for the user to create a category
+		The first thing in the tuple is the value of the category
+		The second thing in the tuple is the actual label in the HTML page.
+	"""
+	product_category = SelectField("Product Category", choices=[('beauty', 'Beauty'), ('electronics', 'Electronics'), ('clothing', 'Clothing'), ('toys', 'Toys'), ('office', 'Office Products')])
+	product_desc = TextAreaField("Product Description", validators=[DataRequired()])
+	product_price = DecimalField("Product Price", validators=[DataRequired()])
+	product_image = StringField("Product Image Link", validators=[DataRequired()])
+	submit = SubmitField('Create Product')
+
+class CreateCategoryForm(FlaskForm):
+	category_name = StringField("Category Name", validators=[DataRequired()])
+	category_description = TextAreaField("Category Description", validators=[DataRequired()])
+	submit = SubmitField('Create Category')
+
+
--- odc_app/routes.py
+++ odc_app/routes.py
@@ -4,8 +4,8 @@ for the purposes of rendering the static pages for testing the desgin.
 """
 
 from odc_app import app
-from odc_app.forms import LoginForm, RegistrationForm
-from flask import render_template, url_for
+from odc_app.forms import LoginForm, RegistrationForm, CreateProductForm, CreateCategoryForm
+from flask import render_template, url_for, redirect
 
 @app.route('/', methods=['GET'])
 @app.route('/index', methods=['GET'])
@@ -25,3 +25,31 @@ def register():
 	if form.validate_on_submit():
 		pass
 	return render_template('register.html', title='Registeration', form=form)
+
+@app.route('/products/new', methods=['GET', 'POST'])
+def create_product():
+	form = CreateProductForm()
+	""" form.product_category.choices must be equal to a list of tuples as seen in the choices attribute in the SelectField in forms.py.
+		This is so we can dynamically load any created categories by the current userfrom the database into 
+		the select field on the html page via a query.
+	"""
+	if form.validate_on_submit():
+		# Save new product to database
+		return redirect(url_for('products'))
+	return render_template('new_product.html', title='Create Product', form=form)
+
+@app.route('/products', methods=['GET'])
+def products():
+	# Query the products of current user
+	# Store returned tuples as list
+	# Pass in list as argument to render_template so that the products can be displayed
+	return render_template('products.html')
+
+@app.route('/categories/new', methods=['GET', 'POST'])
+def create_category():
+	form = CreateCategoryForm()
+	if form.validate_on_submit():
+		# Save new category to database
+		return redirect(url_for('products'))
+	return render_template('new_category.html', title='Create Category', form=form)
+
