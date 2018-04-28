895a3b29e65070c5c1ed943b35ced1bc66bd0433
jacobRettig <jacob13rettig@gmail.com>
2018-04-27 20:30:34 -0500

    created a login, logout, registration system

--- odc_app/sqlHandler.py
+++ odc_app/sqlHandler.py
@@ -1,2 +1,29 @@
 # Primary Author: James Shelley
 # Database models go here
+
+
+
+#checks if email/password combination is valid (returns True/False)
+def isValidLogin(email, password):
+    pass
+
+
+#checks if role exists (returns True/False)
+def emailExists(email):
+    pass
+
+
+#creates a new user if possible and returns True on success, False on failure
+#if no second line of address then it will be None
+def createUser(role, email, password, firstName, lastName, phoneNumber, birthdate, country, region, addressFirstLine, addressSecondLine):
+    pass
+
+
+#gets role from email
+def getRole(email):
+    pass
+
+
+#gets a list containing all the regions in the DB
+def getRegions():
+    pass
