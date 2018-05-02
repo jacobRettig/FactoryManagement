93053af7d739c1678d137b9a9d43115c1784d670
James Shelley <jamesshelley@James-MacBook-Pro.local>
2018-05-02 18:52:23 -0500

    Fixed some bugs

--- odc_app/sqlHandler.py
+++ odc_app/sqlHandler.py
@@ -8,10 +8,13 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import Column, Integer, String, Date, Numeric
 from sqlalchemy import CheckConstraint
 from sqlalchemy import exc
-from base import Session
+from sqlalchemy import ForeignKey
+from sqlalchemy.orm import sessionmaker
 
 engine = create_engine('sqlite:////absolute/path/to/foo.db')
 Base = declarative_base()
+Session = sessionmaker()
+Session.configure(bind=engine)
 session = Session()
 
 #Classes to create tables, based off the schema
@@ -28,7 +31,7 @@ class Country(Base):
 
 class Address(Base):
 	__tablename__ = 'Address'
-	addressID = Column('addressID', Integer(15), primary_key=True) #autoincr automatic? 
+	addressID = Column('addressID', Integer, primary_key=True) #autoincr automatic? 
 	addressLineFirst = Column('addressLineFirst', String(50), nullable=False)
 	addressLineSecond = Column('adressLineSecond', String(50))
 	country = Column('country', String(50), ForeignKey("Country.country"), nullable=False)
@@ -47,7 +50,7 @@ class LogInformation(Base):
 	email = Column('email', String(50), primary_key=True)
 	firstName = Column('firstName', String(75), nullable=False)
 	lastName = Column('lastName', String(75), nullable=False) 
-	addressID = Column('addressID', Integer(15), ForeignKey("Address.addressID"), nullable=False)
+	addressID = Column('addressID', Integer, ForeignKey("Address.addressID"), nullable=False)
 	roleName = Column('roleName', String(20), ForeignKey("Role.roleName"), nullable=False)	
 
 class Product(Base):
@@ -56,7 +59,7 @@ class Product(Base):
 	price = Column('price', Numeric, nullable=False)
 	description = Column('description', String(100))
 	imageData = Column('imageData', String(300), nullable=False)
-	quantity = Column('quantity', Integer(30), nullable=False)
+	quantity = Column('quantity', Integer, nullable=False)
 	productOwner = Column('productOwner', String(50), ForeignKey("LogInformation.email"), primary_key=True)	
 
 class ConfigAttribute(Base):
@@ -75,15 +78,15 @@ class ProductAttribute(Base):
 
 class Warehouse(Base):
 	__tablename__ = 'Warehouse'
-	warehouseID = Column('warehouseID', Integer(20), primary_key=True)
+	warehouseID = Column('warehouseID', Integer, primary_key=True)
 	warehouseOwner = Column('warehouseOwner', String(50), ForeignKey("LogInformation.email"), nullable=False)
 	description = Column('description', String(100))
-	capacity = Column('capacity', Integer(30))
-	addressID = Column('addressID', Integer(15), ForeignKey("Address.addressID"), nullable=False)
+	capacity = Column('capacity', Integer)
+	addressID = Column('addressID', Integer, ForeignKey("Address.addressID"), nullable=False)
 
 class WarehouseStorage(Base):
 	__tablename__ = 'WarehouseStorage'
-	warehouseID = Column('warehouseID', Integer(20), ForeignKey("Warehouse.warehouseID"), primary_key=True)
+	warehouseID = Column('warehouseID', Integer, ForeignKey("Warehouse.warehouseID"), primary_key=True)
 	refillDate = Column('refillDate', Date)
 	#state = Column('state', String(30))
 	price = Column('price', Numeric(20), nullable=False)
@@ -170,4 +173,18 @@ def getRegions():
 		return rows.regionName #will this return a list?
 	except exc.SQLAlchemyError as e:
 		print("getRegions failed with: ", e)
+
+#gets a list of products for the specified warehouse owner
+def getWarehouseProducts(warehouseManager):
+
+	try:
+		rows = session.query(WarehouseStorage).join(Warehouse, WarehouseStorage.warehouseID==Warehouse.warehouseID).filter(Warehouse.warehouseOwner == warehouseManager).all()
+		return rows.productName
+	except exc.SQLAlchemyError as e:
+		print("getWarehouseProducts failed with", e)
+
+
+
+
+
 
